6daa38bf71c3e523b0698625da1fadb3fd5241a5
Freddie <fthompson803@live.com>
2019-10-11 23:59:11 -0400

    adding all mvp

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,10 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import CharacterCard from './CharacterCard.js';
+import SearchForm from './SearchForm.js';
+import styled from 'styled-components';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
 
   const [characters, setCharacters] = useState([]);
+  const [clickyState, setClickyState] = useState(false);
+
+  const setSearchArray = someString => {
+    const items = characters.filter(term =>
+      term.name.toLowerCase().includes(someString)
+    );
+    //console.log(items);
+    setCharacters(items);
+  };
+
+  const resetHit = () => {
+    setClickyState(!clickyState);
+  };
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
@@ -14,7 +30,7 @@ export default function CharacterList() {
         const theChars = await axios.get(
           'https://rickandmortyapi.com/api/character/'
         );
-        console.log(theChars.data.results);
+        //console.log(theChars.data.results);
         setCharacters(theChars.data.results);
       } catch (err) {
         console.log(err);
@@ -22,13 +38,23 @@ export default function CharacterList() {
     }
 
     getEm();
-  }, []);
+  }, [clickyState]);
+
+  const FormatList = styled.section`
+    display: flex;
+    flex-flow: row wrap;
+  `;
 
   return (
-    <section className="character-list">
+    <FormatList className="character-list">
+      <SearchForm
+        func={setSearchArray}
+        reset={resetHit}
+        style="margin-bottom: 1.5rem"
+      />
       {characters.map(character => {
-        return <CharacterCard charac={character} />;
+        return <CharacterCard key={character.id} charac={character} />;
       })}
-    </section>
+    </FormatList>
   );
 }
