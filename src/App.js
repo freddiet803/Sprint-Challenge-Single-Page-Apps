b4897fe756fb3c8beea28691dd7d17042788417c
Freddie <fthompson803@live.com>
2019-10-11 21:40:51 -0400

    editing readme and adding files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from "react";
-import Header from "./components/Header.js";
-
+import React from 'react';
+import Header from './components/Header.js';
+import { Route, Link } from 'react-router-dom';
+import WelcomePage from './components/WelcomePage.js';
+import CharacterList from './components/CharacterList';
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Route exact path="/" component={WelcomePage} />
+      <Route
+        path="/characters"
+        render={props => {
+          return <CharacterList {...props} />;
+        }}
+      />
     </main>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,34 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
 
+  const [characters, setCharacters] = useState([]);
+
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    async function getEm() {
+      try {
+        const theChars = await axios.get(
+          'https://rickandmortyapi.com/api/character/'
+        );
+        console.log(theChars.data.results);
+        setCharacters(theChars.data.results);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    getEm();
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {characters.map(character => {
+        return <CharacterCard charac={character} />;
+      })}
     </section>
   );
 }
